ee75315ee0ad7844577d74e776d5ee5ded9ca7ad
allinbit <allinbit@protonmail.com>
2020-06-22 13:50:12 -0600

    First commit

---
+++ index.js
@@ -0,0 +1,75 @@
+const Web3 = require('web3');
+const Twit = require('twit');
+const dotenv = require('dotenv');
+
+const tbtcABI = require('./tbtc-abi.json');
+const tbtcAddress = '0x1bBE271d15Bb64dF0bc6CD28Df9Ff322F2eBD847';
+
+const address0 = '0x0000000000000000000000000000000000000000';
+const explorerURL = 'https://etherscan.io/tx/';
+
+dotenv.config();
+const providerURL = `wss://mainnet.infura.io/ws/v3/${process.env.INFURA_ID}`;
+
+const WSConfig = {
+    reconnect: {
+      auto: true,
+      delay: 5000,
+      maxAttempts: 5,
+      onTimeout: false
+    }
+  };
+
+const twitterConfig = {
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET,
+    timeout_ms: 1000
+}
+
+const web3 = new Web3(new Web3.providers.WebsocketProvider(providerURL, WSConfig));
+const T = new Twit(twitterConfig);
+
+const tbtcContract = new web3.eth.Contract(tbtcABI, tbtcAddress);
+
+async function init() {
+    tbtcContract.events.Transfer()
+        .on('connected', () => {
+            console.log('Connected to Ethereum Blockchain!');
+        })
+        .on('data', (data) => {
+            processTransaction(data);
+        })
+        .on('error', (error) => {
+            console.log(error);
+        })
+}
+
+async function processTransaction(txData) {
+    let formatedTX;
+    let txURL = `${explorerURL}${txData.transactionHash}`;
+    let {from, to, value} = txData.returnValues;
+    value = web3.utils.fromWei(value, 'ether');
+
+    if (from === address0 && value.includes('99')) {
+        value = Number(value).toFixed(value.length - 6);
+        formatedTX = `${value} #tBTC has been minted! 💎\n ${txURL}`;
+    } else if (to === address0) {
+        formatedTX = `${value} #tBTC has been burned! 🔥\n ${txURL}`;
+    } else {
+        return;
+    }
+    
+    T.post('statuses/update', { status: formatedTX }, function(err, data, response) {
+        if (err) {console.log(err)}
+    })
+}
+
+function postToTwitter(message) {
+    return;
+}
+
+init();
+
+
